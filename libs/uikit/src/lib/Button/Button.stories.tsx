97a41bc8ff9762f9ce4fc49bc194c3b58c9dcb34
Liang, Kui <liang.kui@hotmail.com>
2020-06-23 23:23:19 +0800

    feat(uikit): format button story code

--- libs/uikit/src/lib/Button/Button.stories.tsx
+++ libs/uikit/src/lib/Button/Button.stories.tsx
@@ -1,12 +1,9 @@
+import {text} from '@storybook/addon-knobs'
 import React from 'react'
-import {text, number} from '@storybook/addon-knobs'
 import {Button} from './Button'
 
 export default {title: 'Button'}
 
 export const primary = () => {
-  console.log(number('Padding', 0))
-  console.log(text('Text', 'Click me'))
-
   return <Button>{text('Text', 'Click me')}</Button>
 }
