69ce5b0a9d5b6bca00093875193a4fceb36ac46c
Liang, Kui <liang.kui@hotmail.com>
2020-06-24 00:26:22 +0800

    feat(store): initial store framework

--- libs/store/src/lib/store.ts
+++ libs/store/src/lib/store.ts
@@ -1,3 +1,23 @@
-export function store(): string {
-  return 'store'
+import {BehaviorSubject} from 'rxjs'
+import {scan} from 'rxjs/operators'
+
+export type reducer<S> = (s: S) => S
+
+export function createStore() {
+  const globalState = {}
+
+  const actions$ = new BehaviorSubject(s => s)
+
+  const state$ = actions$.pipe(
+    scan((state, action) => action(state), globalState)
+  )
+
+  function createReducer<S>(fn: reducer<S>) {
+    actions$.next(fn)
+  }
+
+  return {
+    state$,
+    createReducer
+  }
 }
